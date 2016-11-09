d95a7d31f835882a8d13a7c3156213c6cdff2a0f
olegfox <info@lab-developers.com>
2016-11-09 23:27:31 +0300

    Добавлена мобильная версия

--- src/Site/MainBundle/Resources/public/frontend/js/script.js
+++ src/Site/MainBundle/Resources/public/frontend/js/script.js
@@ -1,14 +1,40 @@
 jQuery(function() {
+    var $body = $('body');
+
     //  Инициализация слайдера на главной
-    jQuery('.slider').slick({
-        dots: false,
-        infinite: true,
-        speed: 300,
-        slidesToShow: 1,
-        adaptiveHeight: true,
-        autoplay: true,
-        autoplaySpeed: 5000,
-        pauseOnFocus: false,
-        pauseOnHover: false
+    if (jQuery('.slider').length > 0) {
+        jQuery('.slider').slick({
+            dots: false,
+            infinite: true,
+            speed: 300,
+            slidesToShow: 1,
+            adaptiveHeight: true,
+            autoplay: true,
+            autoplaySpeed: 5000,
+            pauseOnFocus: false,
+            pauseOnHover: false
+        });
+    }
+
+    $('.mobile-menu').click(function() {
+        var className = 'menu--open';
+        if ($body.hasClass(className)) {
+            $body.removeClass(className);
+            setTimeout(function() {
+                $body.find('.menu--background').remove();
+            }, 200);
+        } else {
+            $body.addClass(className);
+            $body.prepend($("<div></div>").addClass('menu--background'));
+            $body.find('.menu--background').click(function() {
+                var className = 'menu--open';
+                if ($body.hasClass(className)) {
+                    $body.removeClass(className);
+                    setTimeout(function() {
+                        $body.find('.menu--background').remove();
+                    }, 200);
+                }
+            });
+        }
     });
 });
